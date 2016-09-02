4911cf4a87b1fad587ccece691bcce03ae69db07
JesseYan <jiayi.yan@kanche.com>
2016-09-02 12:11:34 +0800

    day-1:新增模型，数据，超管

---
+++ minicms/news/migrations/0002_auto_20160902_0324.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('news', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='article',
+            name='published',
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='pub_date',
+            field=models.DateTimeField(default=datetime.datetime(2016, 9, 2, 3, 24, 4, 268972, tzinfo=utc), verbose_name='\u53d1\u8868\u65f6\u95f4', auto_now_add=True),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='update_time',
+            field=models.DateTimeField(auto_now=True, verbose_name='\u66f4\u65b0\u65f6\u95f4', null=True),
+        ),
+    ]
